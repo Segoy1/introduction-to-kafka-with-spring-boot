6504f91253ae73e6cec792111c9dc1366cae9a39
Segoy <segoy.thiel@protonmail.com>
2024-01-29 09:59:50 +0100

    Add JSONDeserializer and OrderCreated Object

--- src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -1,9 +1,14 @@
 package dev.lydtech.dispatch.handler;
 
+import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.service.DispatchService;
+import dev.lydtech.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -23,7 +28,8 @@ class OrderCreatedHandlerTest {
 
     @Test
     void listen() {
-        handler.listen("payload");
-        verify(dispatchService, times(1)).process("payload");
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        handler.listen(testEvent);
+        verify(dispatchService, times(1)).process(testEvent);
     }
 }
--- src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -1,8 +1,11 @@
 package dev.lydtech.dispatch.service;
 
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DispatchServiceTest {
@@ -16,6 +19,7 @@ class DispatchServiceTest {
 
     @Test
     void process() {
-        service.process("payload");
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        service.process(testEvent);
     }
 }
