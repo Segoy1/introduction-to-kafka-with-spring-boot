91e7b9aa806b0c0899da2ee9f21d9aadf9175f11
Segoy <segoy.thiel@protonmail.com>
2024-02-01 09:24:00 +0100

    Dependencies and Classes for Retry and Fail Logic

--- src/main/java/dev/lydtech/dispatch/DispatchConfiguration.java
+++ src/main/java/dev/lydtech/dispatch/DispatchConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -54,5 +55,9 @@ public class DispatchConfiguration {
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 //        config.put("spring.json.add.type.headers", false);
         return new DefaultKafkaProducerFactory<>(config);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
